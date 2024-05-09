026f199d5430435c0d4857efa6bfb0d5f131590c
“Emma <emmalarsen2704@gmail.com>
2024-05-09 11:44:22 +0200

    basic struktur

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles/page.module.css";
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p className={styles.jegerp}>hej</p>
+      </div>
+    </main>
+  );
+}
