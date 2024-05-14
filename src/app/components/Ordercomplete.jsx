e737ecc20388ba6b2276507f44644c46ae977e02
“Emma <emmalarsen2704@gmail.com>
2024-05-14 15:25:03 +0200

    order complete basic content

---
+++ src/app/components/Ordercomplete.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Ordercomplete() {
+  return (
+    <div>
+      <h2>ORDER COMPLETE!</h2>
+      <p>Go check your email for a confirmation</p>
+      <p>See you at Sunset Sound 2024</p>
+    </div>
+  );
+}
+
+export default Ordercomplete;
