8154aea8daf5da644ab760479120f42bb086f79f
Caroline <cave0001@stud.ked.dk>
2024-05-14 15:31:25 +0200

    tick

--- src/app/components/Gearbox.jsx
+++ src/app/components/Gearbox.jsx
@@ -6,6 +6,7 @@ function GearBox({ gearChoice, setGearChoice }) {
       return {
         ...old,
         twotent: Math.max(0, old.twotent - 1),
+        threetent: Math.max(0, old.threetent - 1),
       };
     });
   }
@@ -15,16 +16,25 @@ function GearBox({ gearChoice, setGearChoice }) {
       return {
         ...old,
         twotent: old.twotent + 1,
+        threetent: old.threetent + 1,
       };
     });
   }
   return (
-    <div>
-      <h2>two tent</h2>
-      <button onClick={remove}>-</button>
-      <span>{gearChoice.twotent}</span>
-      <button onClick={add}>+</button>
-    </div>
+    <>
+      <div>
+        <h2>two tent</h2>
+        <button onClick={remove}>-</button>
+        <span>{gearChoice.twotent}</span>
+        <button onClick={add}>+</button>
+      </div>
+      <div>
+        <h2>three tent</h2>
+        <button onClick={remove}>-</button>
+        <span>{gearChoice.threetent}</span>
+        <button onClick={add}>+</button>
+      </div>
+    </>
   );
 }
 export default GearBox;
