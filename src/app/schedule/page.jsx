ecb72067e00a63d82c6d4c612713913c590e9281
Caroline <cave0001@stud.ked.dk>
2024-05-15 20:21:08 +0200

    schedule

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 
-function page() {
-  return <div></div>;
+function schedule({ bandData }) {
+  const [selectDay, setSelectDay] = useState;
+  const days = ["monday", "tuesday", "wednesday", "friday", "saturday", "sunday"];
+  const url = "http://localhost:8080/schedule";
+
+  return (
+    <div>
+      <h1>schedule</h1>
+      <div></div>
+    </div>
+  );
 }
 
-export default page;
+export default schedule;
