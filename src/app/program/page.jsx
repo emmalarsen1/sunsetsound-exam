8339c7bf0df7dfe482023edc8ade15f019574775
NoelWilliam <noelwilliamm@gmail.com>
2024-05-16 15:46:39 +0200

    Opdateret program

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -1,17 +1,50 @@
 import React from "react";
 import Link from "next/link";
+import styles from "./Program.module.css";
 
 export default async function page() {
   const url = "http://localhost:8080/bands";
   const res = await fetch(url);
   const bandData = await res.json();
-  console.log(bandData);
+
+  const topBand = bandData.slice(0, 14);
+  const midBand = bandData.slice(14, 27);
+  const botBand = bandData.slice(27, 69);
+
   return (
     <>
       <h1>Program yehaww</h1>
-      {bandData.map((data) => (
-        <Link href={`/bands/${data.slug}`}>{data.name}</Link>
-      ))}
+      <div className={styles.programwrapper}>
+        <div className={styles.topBandWrapper}>
+          {topBand.map((data) => (
+            <Link key={bandData} href={`/bands/${data.slug}`}>
+              {data.name}
+            </Link>
+          ))}
+        </div>
+        <div className={styles.midBandWrapper}>
+          {midBand.map((data) => (
+            <Link key={bandData} href={`/bands/${data.slug}`}>
+              {data.name}
+            </Link>
+          ))}
+        </div>
+        <div className={styles.botBandWrapper}>
+          {botBand.map((data) => (
+            <Link key={bandData} href={`/bands/${data.slug}`}>
+              {data.name}
+            </Link>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
+
+// {
+//   bandData.map((data) => (
+//     <Link key={bandData} href={`/bands/${data.slug}`}>
+//       {data.name}
+//     </Link>
+//   ));
+// }
