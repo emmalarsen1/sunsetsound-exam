e230a8303d3e506e35680b95478eb25368763678
Caroline <cave0001@stud.ked.dk>
2024-05-20 21:31:26 +0200

    forside og schedule ændringer

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,50 +1,107 @@
 "use client";
 import styles from "../styles/Schedule.module.css";
+import OneSchedule from "../components/OneSchedule";
 import React from "react";
 import { useState, useEffect } from "react";
+import combineData from "@/lib/combineData";
 
-function schedule({ bandData }) {
-  const [selectDay, setSelectDay] = useState("mon");
-  const days = ["monday", "tuesday", "wednesday", "friday", "saturday", "sunday"];
-  const url = "http://localhost:8080/schedule";
-  const [data, setData] = useState([]);
+function Schedule() {
+  const [bands, setbands] = useState();
+  const [day, setDay] = useState("mon");
 
-  // FETCH
+  async function fetchData() {
+    const data = await combineData();
+    setbands(data);
+  }
+
   useEffect(() => {
-    fetch(url + selectDay).then((result) => {
-      result.json().then((resp) => {
-        setData(resp);
-      });
-    });
-  }, [selectDay]);
-
-  const onDayChange = (day) => {
-    setSelectDay(day.substring(0, 3));
-  };
+    fetchData();
+  }, []);
 
   //Time
   const insertTimes = () => {
     const list = [];
     for (let i = 0; i < 24; i += 2) {
-      i < 10 ? list.push(<h2>0{i}:00</h2>) : list.push(<h2>{i}:00</h2>);
+      i < 10 ? list.push(<h2 className={styles.new_times_title}>0{i}:00</h2>) : list.push(<h2 className={styles.new_times_title}>{i}:00</h2>);
     }
     return <div className={styles.new_times}>{list}</div>;
   };
 
   return (
     <div>
-      <h1>schedule</h1>
-      <div className={styles.gridwrapper}>
-        <div className={styles.new_grid}>
+      <h1 className={styles.title}>schedule</h1>
+
+      <div>
+        <button
+          onClick={() => {
+            setDay("mon");
+          }}
+        >
+          MONDAY
+        </button>
+        <button
+          onClick={() => {
+            setDay("tue");
+          }}
+        >
+          TUESDAY
+        </button>
+        <button
+          onClick={() => {
+            setDay("wed");
+          }}
+        >
+          WEDNESDAY
+        </button>
+        <button
+          onClick={() => {
+            setDay("thu");
+          }}
+        >
+          THURSDAY
+        </button>
+        <button
+          onClick={() => {
+            setDay("fri");
+          }}
+        >
+          FRIDAY
+        </button>
+        <button
+          onClick={() => {
+            setDay("sat");
+          }}
+        >
+          SATURDAY
+        </button>
+        <button
+          onClick={() => {
+            setDay("sun");
+          }}
+        >
+          SUNDAY
+        </button>
+      </div>
+
+      <div className={styles.areas}>
+        <div>
           <h3 className={styles.hidden}>TIME</h3>
           <h3>MIDGARD</h3>
           <h3>JOTUNHEIM</h3>
           <h3>VANAHEIM</h3>
+        </div>
+        <div>
           {insertTimes()}
+          {bands && (
+            <div>
+              <OneSchedule data={bands.Midgard} />
+              <OneSchedule data={bands.Jotunheim} />
+              <OneSchedule data={bands.Vanaheim} />
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 }
-
-export default schedule;
+export default Schedule;
---
+++ src/lib/combineData.js
@@ -0,0 +1,22 @@
+import { getData } from "../lib/data";
+
+export default async function combineData() {
+  const allArtist = await getData("bands");
+  const schedule = await getData("schedule");
+
+  for (let stage in schedule) {
+    for (let day in schedule[stage]) {
+      schedule[stage][day].forEach((act) => {
+        const matched = allArtist.find((slot) => slot.name === act.act);
+        if (matched) {
+          act.logo = matched.logo;
+          act.bio = matched.bio;
+          act.members = matched.members;
+          act.genre = matched.genre;
+          act.slug = matched.slug;
+        }
+      });
+    }
+  }
+  return schedule;
+}
