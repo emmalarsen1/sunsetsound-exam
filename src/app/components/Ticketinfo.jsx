f59277207c263db9064d3a8190c67a6cc11fb1ff
“Emma <emmalarsen2704@gmail.com>
2024-05-21 16:19:38 +0200

    opdateret booking page 2.0

--- src/app/components/Ticketinfo.jsx
+++ src/app/components/Ticketinfo.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import styles from "../booking/Booking.module.css";
 
-function Ticketinfo() {
-  return <div></div>;
+function TicketForm({ ticketNumber, fData, onChange }) {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    onChange(name, value);
+  };
+  return (
+    <div className={styles.ticketForm}>
+      <h3>Ticket {ticketNumber}</h3>
+      <div>
+        <form>
+          <fieldset>
+            <label>
+              Fullname:
+              <input
+                type="text"
+                name="fullname"
+                value={fData.fullname || ""}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={fData.email || ""}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Phone-number:
+              <input
+                type="tel"
+                name="phone"
+                value={fData.phone || ""}
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Ticketinfo;
+export default TicketForm;
