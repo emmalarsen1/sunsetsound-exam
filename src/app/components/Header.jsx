80887b0bee7179160ce9634988406bf79733c21e
Caroline <cave0001@stud.ked.dk>
2024-05-23 15:05:53 +0200

    menu

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
+import Image from "next/image";
 
 function Header() {
   return (
     <header>
       <nav className={styles.nav}>
-        <Link href={"/"}>Home</Link>
+        <Link href={"/"} passHref>
+          <Image className={styles.logo} src="ss_logo.svg" alt="logo" width={50} height={50}></Image>
+        </Link>
+
         <div className={styles.headermenu}>
-          <Link href={"/booking"}>Get tickets</Link>
           <Link href={"/program"}>Program</Link>
           <Link href={"/tickets"}>Tickets</Link>
           <Link href={"/schedule"}>Schedule</Link>
