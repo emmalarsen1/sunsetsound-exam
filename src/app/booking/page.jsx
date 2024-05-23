af4de92e14ec066fa3157a1be6224ea3e80f3132
NoelWilliam <noelwilliamm@gmail.com>
2024-05-23 10:19:55 +0200

    Tilføjet klasser til booking

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -9,9 +9,7 @@ import Billingform from "../components/Billingform";
 import TicketForm from "../components/Ticketinfo";
 
 function Booking() {
-  const fee = [
-    { name: "Fixed booking fee", price: "99", id: 0, type: "fee", amount: 1 },
-  ];
+  const fee = [{ name: "Fixed booking fee", price: "99", id: 0, type: "fee", amount: 1 }];
 
   const [data, setData] = useState(null);
 
@@ -58,8 +56,8 @@ function Booking() {
     });
   };
   return (
-    <div className={styles.bookingmain}>
-      <ol className={styles.breadcrumbswrapper}>
+    <div className={styles.bookingMain}>
+      <ol className={styles.breadCrumbsWrapper}>
         <li>
           {/* onClick => setPage sætter siden til det korrekte side ud fra index  */}
           {/*disabled sørger for at brødkrumme kun er brugbar tilbage og ikke frem */}
@@ -88,16 +86,12 @@ function Booking() {
           </button>
         </li>
       </ol>
-      <div className={styles.bookingwrapper}>
+      <div className={styles.bookingWrapper}>
         <section>
           {page === 0 && (
             <div>
               {" "}
-              <Ticketsbox
-                ticketChoice={ticketChoice}
-                setTicketChoice={setTicketChoice}
-              />{" "}
-              <Gearbox gearChoice={gearChoice} setGearChoice={setGearChoice} />{" "}
+              <Ticketsbox ticketChoice={ticketChoice} setTicketChoice={setTicketChoice} /> <Gearbox gearChoice={gearChoice} setGearChoice={setGearChoice} />{" "}
             </div>
           )}
           {page === 1 && (
@@ -110,40 +104,21 @@ function Booking() {
               {/* ticketNumber={Regular ${i + 1}} dette prop gives til hver TicketForm for at give hvert komponent et navn "Ticket 1" og "Ticket 2" eksempelvis  */}
               {/* Efterfølgende sker det samme med VIP  */}
               {Array.from({ length: ticketChoice.regular }, (_, i) => (
-                <TicketForm
-                  key={`regular${i}`}
-                  ticketNumber={`Regular ${i + 1}`}
-                  fData={formData.regular[i] || {}}
-                  onChange={(field, value) =>
-                    handleFormDataChange("regular", i, field, value)
-                  }
-                />
+                <TicketForm key={`regular${i}`} ticketNumber={`Regular ${i + 1}`} fData={formData.regular[i] || {}} onChange={(field, value) => handleFormDataChange("regular", i, field, value)} />
               ))}
               {Array.from({ length: ticketChoice.vip }, (_, i) => (
-                <TicketForm
-                  key={`vip${i}`}
-                  ticketNumber={`VIP ${i + 1}`}
-                  fData={formData.vip[i] || {}}
-                  onChange={(field, value) =>
-                    handleFormDataChange("vip", i, field, value)
-                  }
-                />
+                <TicketForm key={`vip${i}`} ticketNumber={`VIP ${i + 1}`} fData={formData.vip[i] || {}} onChange={(field, value) => handleFormDataChange("vip", i, field, value)} />
               ))}
             </div>
           )}
-          {page === 2 && (
-            <Availablespots data={data} ticketTotal={ticketTotal} />
-          )}
+          {page === 2 && <Availablespots data={data} ticketTotal={ticketTotal} />}
           {page === 3 && <Billingform></Billingform>}
           {page === 4 && <Ordercomplete />}
           {/* Sørger for ikke at vise knapperne på confirmed siden  */}
           {page !== 4 && (
             <div>
               <button onClick={() => setPage((o) => o - 1)}>Back</button>
-              <button
-                className={styles.nextbutton}
-                onClick={() => setPage((o) => o + 1)}
-              >
+              <button className={styles.nextbutton} onClick={() => setPage((o) => o + 1)}>
                 Next
               </button>
             </div>
@@ -151,39 +126,31 @@ function Booking() {
         </section>
         {/* Sørger for ikke at vise kurven på confirmed siden  */}
         {page !== 4 && (
-          <section>
+          <section className={styles.basketWrapper}>
             <h2>Basket</h2>
+            <div className={styles.feeTickets}>
+              {ticketChoice.regular + ticketChoice.vip > 0 && <h3>Ticket(s):</h3>}
+              {ticketChoice.regular > 0 && <p>Regular Ticket: {ticketChoice.regular}x 799,-</p>}
+              {ticketChoice.vip > 0 && <p>VIP Ticket: {ticketChoice.vip}x 1299,-</p>}
+            </div>
+            <div className={styles.feeTents}>
+              {gearChoice.twotent + gearChoice.threetent > 0 && <h3>Tent(s):</h3>}
+              {gearChoice.twotent > 0 && <p>2-person tent: {gearChoice.twotent}x 299,-</p>}
+              {gearChoice.threetent > 0 && <p>3-person tent: {gearChoice.threetent}x 399,-</p>}
+            </div>
+            <p className={styles.feeTotal}>
+              Total: {fee[0].amount * fee[0].price + ticketChoice.regular * 799 + ticketChoice.vip * 1299 + gearChoice.twotent * 299 + gearChoice.threetent * 399}
+              ,-
+            </p>
             {fee.map(
               (item) =>
                 item.amount > 0 && (
-                  <div className={styles.feewrapper} key={item.id}>
+                  <div className={styles.feeFixed} key={item.id}>
                     <p>{item.name}&nbsp;</p>
                     <p>{item.price},-</p>
                   </div>
                 )
             )}
-
-            {ticketChoice.regular > 0 && (
-              <p>Regular Ticket: {ticketChoice.regular}x 799,-</p>
-            )}
-            {ticketChoice.vip > 0 && (
-              <p>VIP Ticket: {ticketChoice.vip}x 1299,-</p>
-            )}
-            {gearChoice.twotent > 0 && (
-              <p>2-person tent: {gearChoice.twotent}x 299,-</p>
-            )}
-            {gearChoice.threetent > 0 && (
-              <p>3-person tent: {gearChoice.threetent}x 399,-</p>
-            )}
-            <p>
-              Total:{" "}
-              {fee[0].amount * fee[0].price +
-                ticketChoice.regular * 799 +
-                ticketChoice.vip * 1299 +
-                gearChoice.twotent * 299 +
-                gearChoice.threetent * 399}
-              ,-
-            </p>
           </section>
         )}
       </div>
