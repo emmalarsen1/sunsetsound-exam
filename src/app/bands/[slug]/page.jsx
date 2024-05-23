d85f776351da25188e95af04d433dcb1b22545f8
NoelWilliam <noelwilliamm@gmail.com>
2024-05-23 13:18:42 +0200

    Fikset global main og lavet lidt på bands siden

--- src/app/bands/[slug]/page.jsx
+++ src/app/bands/[slug]/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { dummyData } from "@/app/data/dummyData";
 import Image from "next/image";
+import styles from "./Bandname.module.css";
 
 export async function generateStaticParams() {
   // const res = await fetch("https://broken-tinted-wombat.glitch.me/bands");
@@ -13,6 +14,18 @@ export async function generateStaticParams() {
 
   return paths;
 }
+function formatBandMembers(members) {
+  const len = members.length;
+  if (len === 0) {
+    return "";
+  } else if (len === 1) {
+    return members[0];
+  } else if (len === 2) {
+    return `${members[0]} & ${members[1]}`;
+  } else {
+    return `${members.slice(0, -1).join(", ")} & ${members[len - 1]}`;
+  }
+}
 
 export default async function page({ params }) {
   const { slug } = params;
@@ -24,16 +37,23 @@ export default async function page({ params }) {
   const bandData = await res.json();
   const oneBand = bandData.filter((oneRule) => oneRule.slug === slug);
   const data = oneBand[0];
+  const formattedMembers = formatBandMembers(data.members);
   console.log(data);
 
   return (
     <>
       <section>
-        <Image src={data.logo && !data.logo.startsWith("https") ? `http://localhost:8080/logos/${data.logo}` : data.logo} alt="cover of the band" width={160} height={160} />
-        <h1>{data.name}</h1>
-        <p>{data.members}</p>
-        <p>{data.genre}</p>
-        <p>{data.bio}</p>
+        <h1 className={styles.bandName}>{data.name}</h1>
+        <div className={styles.bandWrapper}>
+          <div>
+            <p>Dag, Tid, Scene</p>
+            <p>{data.bio}</p>
+            <p>
+              Let {data.name} take you on a {data.genre}-journey, guided by the incredible talents of {formattedMembers}.
+            </p>
+          </div>
+          <Image className={styles.bandImage} src={data.logo && !data.logo.startsWith("https") ? `http://localhost:8080/logos/${data.logo}` : data.logo} alt="cover of the band" width={160} height={160} />
+        </div>
       </section>
     </>
   );
