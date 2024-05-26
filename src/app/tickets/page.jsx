6ac49722fcaf65b7d2ba3b2c5f6507d20bfc159c
“Emma <emmalarsen2704@gmail.com>
2024-05-26 14:12:36 +0200

    lørdags arbejde

--- src/app/tickets/page.jsx
+++ src/app/tickets/page.jsx
@@ -1,49 +1,67 @@
 import React from "react";
 import styles from "./Tickets.module.css";
+import TicketCard from "../components/TicketCard";
+import Link from "next/link";
 
 function page() {
   return (
     <>
-      <div className={styles.mainSections}>
-        <section className={styles.eachSection}>
-          <div className={styles.generalWrapper}>
-            <p>Regular Ticket:</p>
-            <p>
-              A regular ticket grants access to all general admission areas of
-              the summer music festival, including the main stage performances,
-              food and drink vendors, and general amenities. It ensures a
-              vibrant festival experience with access to all public zones.
-            </p>
-          </div>
-          <div className={styles.generalWrapper}>
-            <p>VIP Ticket:</p>
-            <p>
-              A VIP ticket offers exclusive perks, including fast-track entry,
-              access to premium viewing areas, and a dedicated lounge with
-              complimentary refreshments. VIP guests also enjoy private
-              restrooms and priority access to select activities and events.
-            </p>
-          </div>
-        </section>
-        <section className={styles.eachSection}>
-          <div className={styles.generalWrapper}>
-            <p>Two-Person Tent:</p>
-            <p>
-              A two-person tent provides cozy shelter with easy setup and
-              weather-resistant materials, ideal for couples or friends.
-            </p>
-          </div>
-          <div className={styles.generalWrapper}>
-            <p>Three-Person Tent:</p>
-            <p>
-              A three-person tent offers extra space, quick setup, and durable
-              construction, perfect for small groups or those needing more room.
-            </p>
-          </div>
-        </section>
+      {" "}
+      <h1 className={`globalHeader`}>Tickets</h1>
+      <div className={styles.indexButWrap}>
+        <Link className={styles.indexButton} href={"/booking"}>
+          Get Tickets
+        </Link>
+      </div>
+      <div>
+        <div className={styles.eachSection}>
+          <TicketCard
+            name="REGULAR"
+            desc="Full access, all week, all year"
+            price="799 DKK"
+          ></TicketCard>
+          <TicketCard
+            name="VIP"
+            desc="Full access with extra perks"
+            price="1299 DKK"
+          ></TicketCard>
+        </div>
+        <h2 className={styles.campingTitle}>Camping options</h2>
+        <div className={styles.eachSection}>
+          <TicketCard
+            name="2-person-tent"
+            desc=" A two-person tent installed by Sunset Sound-crew"
+            price="299 DKK"
+          ></TicketCard>
+          <TicketCard
+            name="3-person-tent"
+            desc="A three-person tent installed by Sunset Sound-crew"
+            price="399 DKK"
+          ></TicketCard>
+        </div>
       </div>
     </>
   );
 }
 
 export default page;
+
+{
+  /* 
+  
+    <p>Two-Person Tent:</p>
+    <p>
+      A two-person tent provides cozy shelter with easy setup and
+      weather-resistant materials, ideal for couples or friends.
+    </p>
+  </div>
+  <div className={styles.generalWrapper}>
+    <p>Three-Person Tent:</p>
+    <p>
+      A three-person tent offers extra space, quick setup, and durable
+      construction, perfect for small groups or those needing more room.
+    </p>
+  </div>
+</section>
+</div> */
+}
