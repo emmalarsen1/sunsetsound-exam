6d679401d9d79ed29b0d6a971692d1a5fc29ce58
Caroline <cave0001@stud.ked.dk>
2024-05-28 10:23:08 +0200

    not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className={styles.center}>
-      <h1>404</h1>
-      <h3>OPS! SOMETHING WENT WORNG</h3>
+      <h1 className={`globalHeader`}>404</h1>
+      <h3>OPS! SOMETHING WENT WRONG</h3>
       <p>Go back to home page</p>
       <button>
         <Link className={styles.indexButton} href={"/"}>
