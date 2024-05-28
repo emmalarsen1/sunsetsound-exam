7ef265676b40eab6034aa92dcb4aa5c8e077c6ac
Caroline <cave0001@stud.ked.dk>
2024-05-28 17:04:52 +0200

    ændringer

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -18,21 +18,21 @@ export default async function page() {
       <div className={styles.programwrapper}>
         <div className={styles.topBandWrapper}>
           {topBand.map((data) => (
-            <Link className={styles.topBandStyle} key={bandData} href={`/bands/${data.slug}`}>
+            <Link className={styles.topBandStyle} key={data.name} href={`/bands/${data.slug}`}>
               {data.name}
             </Link>
           ))}
         </div>
         <div className={styles.midBandWrapper}>
           {midBand.map((data) => (
-            <Link className={styles.midBandStyle} key={bandData} href={`/bands/${data.slug}`}>
+            <Link className={styles.midBandStyle} key={data.name} href={`/bands/${data.slug}`}>
               {data.name}
             </Link>
           ))}
         </div>
         <div className={styles.botBandWrapper}>
           {botBand.map((data) => (
-            <Link className={styles.botBandStyle} key={bandData} href={`/bands/${data.slug}`}>
+            <Link className={styles.botBandStyle} key={data.name} href={`/bands/${data.slug}`}>
               {data.name}
             </Link>
           ))}
