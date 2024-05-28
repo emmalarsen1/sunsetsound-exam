7ae23cd4f57b57e150f8b9a19022aa442e361b42
Caroline <cave0001@stud.ked.dk>
2024-05-28 15:38:19 +0200

    knap

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/Button.module.css";
 function Button({ href, buttontext, color }) {
   const buttonColor = color === "primary" ? styles.yellow : styles.pink;
   return (
-    <div>
+    <div className={styles.button_a}>
       <Link className={`${styles.buttonstyle} ${buttonColor}`} href={href}>
         {buttontext}
       </Link>
