cdeb8bd5f04f9ddd8c1c6747e179ca3fb6ee8733
“Emma <emmalarsen2704@gmail.com>
2024-05-29 15:30:29 +0200

    begyndelse på mediaquery booking

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -7,14 +7,13 @@ import Ordercomplete from "../components/bookingflow/Ordercomplete";
 import Billingform from "../components/bookingflow/Billingform";
 import GetTicketInfo from "../components/bookingflow/GetTicketInfo";
 import ToolTip from "../components/bookingflow/ToolTip";
+import { useMediaQuery } from "react-responsive";
 
 function Booking() {
   const fee = [
     { name: "Fixed booking fee", price: "99", id: 0, type: "fee", amount: 1 },
   ];
-
   const [data, setData] = useState(null);
-
   useEffect(() => {
     // fetch("https://broken-tinted-wombat.glitch.me/available-spots")
     fetch("http://localhost:8080/available-spots")
@@ -45,22 +44,10 @@ function Booking() {
     greenCamping: false,
   });
 
-  // const [formData, setFormData] = useState({
-  //   regular: [],
-  //   vip: [],
-  // });
-
-  // const handleFormDataChange = (type, index, field, value) => {
-  //   setFormData((prev) => {
-  //     const newFormData = { ...prev };
-  //     if (!newFormData[type][index]) {
-  //       newFormData[type][index] = {};
-  //     }
-  //     newFormData[type][index][field] = value;
-  //     return newFormData;
-  //   });
-  // };
-
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
+  const isTablet = useMediaQuery({
+    query: "(min-width: 601px) and (max-width: 1024px)",
+  });
   return (
     <>
       <h1 className={`globalHeader`}>Tickets</h1>
@@ -94,94 +81,122 @@ function Booking() {
             </button>
           </li>
         </ol>
-        <div className={styles.bookingWrapper}>
-          <section className={styles.basketWrapper}>
-            {page === 0 && (
-              <div>
-                {" "}
-                <Ticketsbox
-                  setPage={setPage}
-                  ticketChoice={ticketChoice}
-                  setTicketChoice={setTicketChoice}
-                  gearChoice={gearChoice}
-                  setGearChoice={setGearChoice}
-                />
-              </div>
-            )}
-            {page === 1 && (
-              <div>
-                <div className={styles.bookingSub}>
+        <div
+          className={
+            isMobile
+              ? styles.mobileWrapper
+              : isTablet
+              ? styles.tabletWrapper
+              : styles.desktopWrapper
+          }
+        >
+          <section>
+            <div
+              className={
+                isMobile
+                  ? styles.sectionWrapperMobile
+                  : isTablet
+                  ? styles.sectionWrapperTablet
+                  : styles.sectionWrapperDesktop
+              }
+            >
+              {page === 0 && (
+                <div>
+                  {" "}
+                  <Ticketsbox
+                    setPage={setPage}
+                    ticketChoice={ticketChoice}
+                    setTicketChoice={setTicketChoice}
+                    gearChoice={gearChoice}
+                    setGearChoice={setGearChoice}
+                  />
+                </div>
+              )}
+              {page === 1 && (
+                <div>
                   <p>Ticket(s) Info</p>
                   <ToolTip ToolTipText="Fill out the information of each ticketholder"></ToolTip>
+                  <div>
+                    <GetTicketInfo
+                      setPage={setPage}
+                      ticketChoice={ticketChoice}
+                    ></GetTicketInfo>
+                  </div>
                 </div>
-                <GetTicketInfo
+              )}
+              {page === 2 && (
+                <Availablespots
                   setPage={setPage}
-                  ticketChoice={ticketChoice}
-                ></GetTicketInfo>
-              </div>
-            )}
-            {page === 2 && (
-              <Availablespots
-                setPage={setPage}
-                data={data}
-                ticketTotal={ticketTotal}
-              />
-            )}
-            {page === 3 && <Billingform setPage={setPage}></Billingform>}
-            {page === 4 && <Ordercomplete />}
-            {/* Sørger for ikke at vise knapperne på confirmed siden  */}
+                  data={data}
+                  ticketTotal={ticketTotal}
+                />
+              )}
+              {page === 3 && <Billingform setPage={setPage}></Billingform>}
+              {page === 4 && <Ordercomplete />}
+              {/* Sørger for ikke at vise knapperne på confirmed siden  */}
+            </div>
           </section>
           {/* Sørger for ikke at vise kurven på confirmed siden  */}
           {page !== 4 && (
-            <section className={styles.basketWrapper}>
-              <div className={styles.basketContent}>
-                <p className={styles.basketTitle}>sunset sound</p>
-                <p className={styles.basketSubtitle}>Basket</p>
-                <hr />
-                <div className={styles.feeTickets}>
-                  {ticketChoice.regular + ticketChoice.vip > 0 && (
-                    <p>Ticket(s):</p>
-                  )}
-                  {ticketChoice.regular > 0 && (
-                    <p>Regular Ticket: {ticketChoice.regular}x 799,-</p>
-                  )}
-                  {ticketChoice.vip > 0 && (
-                    <p>VIP Ticket: {ticketChoice.vip}x 1299,-</p>
-                  )}
-                </div>
-                <div className={styles.feeTents}>
-                  {gearChoice.twotent + gearChoice.threetent > 0 && (
-                    <p>Tent(s):</p>
-                  )}
-                  {gearChoice.twotent > 0 && (
-                    <p>2-person tent: {gearChoice.twotent}x 299,-</p>
-                  )}
-                  {gearChoice.threetent > 0 && (
-                    <p>3-person tent: {gearChoice.threetent}x 399,-</p>
-                  )}
-                  {gearChoice.greenCamping && <p>Green Camping: 1x 250,-</p>}
-                </div>
-                <hr />
-                <div>
-                  <p className={styles.feeTotal}>
-                    Total:{" "}
-                    {fee[0].amount * fee[0].price +
-                      ticketChoice.regular * 799 +
-                      ticketChoice.vip * 1299 +
-                      gearChoice.twotent * 299 +
-                      gearChoice.threetent * 399 +
-                      gearChoice.greenCamping * 250}
-                    ,-
-                  </p>
-                  {fee.map(
-                    (item) =>
-                      item.amount > 0 && (
-                        <div className={styles.feeFixed} key={item.id}>
-                          <p>{item.name}&nbsp;</p>
-                          <p>{item.price},-</p>
-                        </div>
-                      )
-                  )}
+            <section>
+              <div
+                className={
+                  isMobile
+                    ? styles.sectionWrapperMobile
+                    : isTablet
+                    ? styles.sectionWrapperTablet
+                    : styles.sectionWrapperDesktop
+                }
+              >
+                <div className={styles.basketContent}>
+                  <p className={styles.basketTitle}>sunset sound</p>
+                  <p className={styles.basketSubtitle}>Basket</p>
+                  <hr />
+                  <div className={styles.feeTickets}>
+                    {ticketChoice.regular + ticketChoice.vip > 0 && (
+                      <p>Ticket(s):</p>
+                    )}
+                    {ticketChoice.regular > 0 && (
+                      <p>Regular Ticket: {ticketChoice.regular}x 799,-</p>
+                    )}
+                    {ticketChoice.vip > 0 && (
+                      <p>VIP Ticket: {ticketChoice.vip}x 1299,-</p>
+                    )}
+                  </div>
+                  <div className={styles.feeTents}>
+                    {gearChoice.twotent + gearChoice.threetent > 0 && (
+                      <p>Tent(s):</p>
+                    )}
+                    {gearChoice.twotent > 0 && (
+                      <p>2-person tent: {gearChoice.twotent}x 299,-</p>
+                    )}
+                    {gearChoice.threetent > 0 && (
+                      <p>3-person tent: {gearChoice.threetent}x 399,-</p>
+                    )}
+                    {gearChoice.greenCamping && <p>Green Camping: 1x 250,-</p>}
+                  </div>
+                  <hr />
+                  <div>
+                    <p className={styles.feeTotal}>
+                      Total:{" "}
+                      {fee[0].amount * fee[0].price +
+                        ticketChoice.regular * 799 +
+                        ticketChoice.vip * 1299 +
+                        gearChoice.twotent * 299 +
+                        gearChoice.threetent * 399 +
+                        gearChoice.greenCamping * 250}
+                      ,-
+                    </p>
+                    {fee.map(
+                      (item) =>
+                        item.amount > 0 && (
+                          <div className={styles.feeFixed} key={item.id}>
+                            <p>{item.name}&nbsp;</p>
+                            <p>{item.price},-</p>
+                          </div>
+                        )
+                    )}
+                  </div>
                 </div>
               </div>
             </section>
