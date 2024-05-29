810718f892d932e484d9f60c7c50dae4ed40742e
NoelWilliam <noelwilliamm@gmail.com>
2024-05-29 10:07:16 +0200

    asdad

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,6 @@ import "./styles/reset.css";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import Link from "next/link";
-import { Libre_Baskerville } from "next/font/google";
 
 export const metadata = {
   title: "Sunset sound festival",
