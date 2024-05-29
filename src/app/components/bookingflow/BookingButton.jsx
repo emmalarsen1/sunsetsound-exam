9c54d2d1fcaa706d75a278f53c7ac21633ab71e4
Caroline <cave0001@stud.ked.dk>
2024-05-29 11:51:47 +0200

    farver

--- src/app/components/bookingflow/BookingButton.jsx
+++ src/app/components/bookingflow/BookingButton.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "@/app/styles/bookingstyles/ConfirmButton.module.css";
 
-function BookingButton({ buttontext, onClick, disabled }) {
+function BookingButton({ buttontext, onClick, disabled, color }) {
+  const buttonColor = color === "primary" ? styles.yellow : styles.pink;
   return (
-    <button className={styles.BookingBtn} onClick={onClick} disabled={disabled}>
+    <button className={`${styles.BookingBtn} ${buttonColor}`} onClick={onClick} disabled={disabled}>
       {buttontext}
     </button>
   );
